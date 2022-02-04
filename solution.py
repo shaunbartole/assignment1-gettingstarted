4a774c378d6c43d2c443dfd968e4e64a320b08ad
shaunbartole <82050408+shaunbartole@users.noreply.github.com>
2022-02-04 09:02:35 -0500

    update

--- solution.py
+++ solution.py
@@ -10,7 +10,7 @@ def welcome_assignment_answers(question):
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
     elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer = "answer"
+        answer = "MTLS"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "Yes"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
